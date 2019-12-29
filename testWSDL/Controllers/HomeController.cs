0a449ff6dd5a2c2c04a162371f989d9c3ae9650d
mike <zhendongtt@gmail.com>
2019-12-29 11:55:30 -0500

    Add project files.

---
+++ testWSDL/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel.Description;
+using System.Collections.ObjectModel;
+using HelixToolkit.Wpf;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace testWSDL.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        
+            
+            
+
+
+
+
+        public ContentResult Index()
+        {
+                                 
+                return Content("hello, tang");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ testWSDL/WSDL/ParseWsdlConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testWSDL.WSDL
+{
+    /// <summary>
+    /// This class will read a configure file which includes a URI list of web services, and generate a List which 
+    /// includes the result of parse
+    /// </summary>
+    
+    public class ParseWsdlConfig
+    {
+        /// read a file according to given file path
+        
+        
+        
+        ///parse the file 
+
+    }
+}
